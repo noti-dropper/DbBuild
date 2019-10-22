e4c8ffa080886783bbb49264ae13cbebcbdb1b47
ide127 <a71825485@gmail.com>
2019-10-22 16:22:29 +0900

    테이블 두개 만드는거 완료

--- app/src/main/java/com/example/dblogic/MainActivity.java
+++ app/src/main/java/com/example/dblogic/MainActivity.java
@@ -1,8 +1,9 @@
 package com.example.dblogic;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //NotiTable을 생성하고 객체를 가져옵니다.
+        NotiDBHelper NotiDBHelper = new NotiDBHelper(this);
+        SQLiteDatabase NotiDB = NotiDBHelper.getWritableDatabase();
+
+        //NounTable을 생성하고 객체를 가져옵니다.
+        NounDBHelper NounDBHelper = new NounDBHelper(this);
+        SQLiteDatabase NounDB = NounDBHelper.getWritableDatabase();
     }
 }
